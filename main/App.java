2f31062f29659cfbb1cd6a5ce21d8d22f9556a67
jessejunior02 <jessepcarvalhojunior02@gmail.com>
2025-01-14 13:59:50 -0300

    test: Testando a funcionalidade de cadastro

--- main/App.java
+++ main/App.java
@@ -1,5 +1,8 @@
 package main;
 
+import main.dao.cliente.ClienteDAO;
+import main.model.ClienteModel;
+
 /**
  *
  * @author jessejunior
@@ -10,7 +13,23 @@ public class App {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        String nome = "Jesse";
+        String cpf = "12345678911";
+        String genero = "M";
+        String celular = "(00)00000-0000";
+        String empresa = "JPTech";
+        String area = "Desenvolvimento";
+        
+        ClienteModel cliM = new ClienteModel();
+        cliM.setNome(nome);
+        cliM.setCpf(cpf);
+        cliM.setGenero(genero);
+        cliM.setCelular(celular);
+        cliM.setEmpresa(empresa);
+        cliM.setArea(area);
+        
+        ClienteDAO cliD = new ClienteDAO();
+        cliD.cadastrarCliente(cliM);
     }
     
 }
